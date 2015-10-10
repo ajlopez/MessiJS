ec55d66d2e06c8a19a99e9e13153a29e6545a364
ajlopez <webmaster@ajlopez.com>
2015-10-10 10:40:37 -0300

    Post message to route using context

--- test/post.js
+++ test/post.js
@@ -17,3 +17,22 @@ exports['post message to route'] = function (test) {
         test.done();
     });
 }
+
+exports['post message to route using context'] = function (test) {
+    test.async();
+    
+    var mproc = messi();
+    
+    mproc.route('myroute', function (message, context) {
+        test.ok(context);
+        test.equal(typeof context, 'object');
+        return message + 1;
+    });
+    
+    mproc.post('myroute', 1, function (err, message) {
+        test.equal(err, null);
+        test.ok(message);
+        test.equal(message, 2);
+        test.done();
+    });
+}
