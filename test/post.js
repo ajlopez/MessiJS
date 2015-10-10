5ca66290d294380931ee7a7b1b45686f2332974f
ajlopez <webmaster@ajlopez.com>
2015-10-10 10:46:58 -0300

    Post message to route using context and callback

--- test/post.js
+++ test/post.js
@@ -36,3 +36,25 @@ exports['post message to route using context'] = function (test) {
         test.done();
     });
 }
+
+
+exports['post message to route using context and callback'] = function (test) {
+    test.async();
+    
+    var mproc = messi();
+    
+    mproc.route('myroute', function (message, context, cb) {
+        test.ok(context);
+        test.equal(typeof context, 'object');
+        test.ok(cb);
+        test.equal(typeof cb, 'function');
+        cb(null, message + 1);
+    });
+    
+    mproc.post('myroute', 1, function (err, message) {
+        test.equal(err, null);
+        test.ok(message);
+        test.equal(message, 2);
+        test.done();
+    });
+}
