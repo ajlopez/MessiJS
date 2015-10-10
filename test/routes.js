ff5a45f76413ea9a84544e2cbf44e1d8e4a1c2cf
ajlopez <webmaster@ajlopez.com>
2015-10-10 11:59:41 -0300

    Additional test, create route using binary function

--- test/routes.js
+++ test/routes.js
@@ -16,3 +16,23 @@ exports['create route from unary function'] = function (test) {
         test.done();
     });
 };
+
+exports['create route from binary function'] = function (test) {
+    var fn = routes.create(function (message, context) { 
+        test.ok(context);
+        test.equal(typeof context, 'object');
+        return message + 1;
+    });
+    
+    test.ok(fn);
+    test.equal(typeof fn, 'function');
+    test.equal(fn.length, 3);
+    
+    test.async();
+    
+    fn(1, {}, function (err, result) {
+        test.equal(err, null);
+        test.equal(result, 2);
+        test.done();
+    });
+};
