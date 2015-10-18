d2f80bf8e77be947046c6eb1a04dba908012c518
ajlopez <webmaster@ajlopez.com>
2015-10-18 09:50:57 -0300

    Additional test, create route from an array of objects

--- test/dsl.js
+++ test/dsl.js
@@ -1,7 +1,7 @@
 
 var messi = require('..');
 
-exports['create transform route'] = function (test) {
+exports['create transform route using an object'] = function (test) {
     var mproc = messi();
     
     mproc.route("process", {
@@ -14,6 +14,29 @@ exports['create transform route'] = function (test) {
         test.equal(err, null);
         test.ok(data);
         test.equal(data, 2);
+        test.done();
+    });
+};
+
+exports['create transform route using an array of objects'] = function (test) {
+    var mproc = messi();
+    
+    mproc.route("process", [
+        {
+            transform: "message + 2"
+        }
+        ,
+        {
+            transform: "message * 5"
+        }
+    ]);
+    
+    test.async();
+    
+    mproc.post("process", 1, function (err, data) {
+        test.equal(err, null);
+        test.ok(data);
+        test.equal(data, 15);
         test.done();
     });
 };
